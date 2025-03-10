bb00c9042511009900459da467a966e6add6c844
Kevin Charnock <kevinshuldiner@gmail.com>
2025-03-10 19:37:55 -0300

    maps

--- dart_application_assincron_api/bin/main.dart
+++ dart_application_assincron_api/bin/main.dart
@@ -4,7 +4,10 @@ import 'dart:convert';
 
 void main(){
 
-requestData();
+// requestData();
+// requestDataAsync();
+// requestDataBooksAsync();
+testeMap("Haley");
 }
 
 //Func pra fazer a requisição do gist
@@ -27,4 +30,41 @@ requestData(){
    print(carla["balance"]);
 
   },);
+}
+ 
+//Funcao de fato assincrona
+//await sempre depois de uma função que retorna future
+requestDataAsync () async {
+  const String url = "https://gist.githubusercontent.com/kevincharnock/194434b04e545cac42eb5273503604b7/raw/3e7e32f3f123e4c19e9366f6bdc2fec9b8f2eb8b/accounts.json";
+  Response response= await get(Uri.parse(url));
+  print(json.decode(response.body)[3]);
+} 
+
+//Step1
+requestDataBooksAsync () async {
+  const String url = "https://raw.githubusercontent.com/alura-cursos/dart_assincronismo_api/aula05/.json/books.json";
+  const String url1 = "https://raw.githubusercontent.com/alura-cursos/dart_assincronismo_api/aula05/.json/recipes.json";
+
+  Response response = await get(Uri.parse(url1));
+  
+  //Usando decoder
+  // print(json.decode(response.body)[0]);
+  print(response.body);
+}
+
+sendDataAsync (Map<String, dynamic> data) async {
+  const String url = "https://gist.githubusercontent.com/kevincharnock/194434b04e545cac42eb5273503604b7/raw/3e7e32f3f123e4c19e9366f6bdc2fec9b8f2eb8b/accounts.json";
+  Response response = await post(Uri.parse(url), body: json.encode(data));
+  print(response.body);
+
+}
+
+testeMap(String nome){
+  Map<String, int> idadePessoas = {
+  'Haley': 22,
+  'Abigail': 19,
+  'Sebastian': 20, 
+};
+ return print(idadePessoas[nome]);
+
 }
